8027338bd8e40a3a59030696caf92bf9e0450ffa
maxmudriak <maxim.mudryak@gmail.com>
2017-12-06 16:27:06 +0200

    first commmit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+;(function(){
+	"use strict";
+
+//Жалкі попитки зробити цикл який просить ввести число більше ніж в аргументі функції
+
+
+	// function getNamber(){
+
+	// 	var i = prompt ("Введіть число", 0),
+	// 	numb = prompt ("Введіть число більше", 0);
+
+	// 	while (i < numb) {
+	// 	  i = prompt ("Введіть число більше", 0);
+	// 	}
+
+	// 	// var firstNumb = prompt ("Введіть число", 0),
+	// 	// 	numb = prompt ("Введіть число більше", 0);
+
+	// 	// while(numb > firstNumb && num != null){
+	// 	// 	numb = prompt ("Введіть число більше");
+	// 	// 	console.log(numb);
+	// 	// 	console.log(firstNumb);
+	// 	// }
+	// }
+	// 	document.addEventListener("DOMContentLoaded", getNamber);
+
+
+
+
+
+	// var firstNumb = prompt ("Введіть число"),
+	// 	numb = prompt ("Введіть число більше ніж "+ firstNumb);
+	// for (numb; numb < firstNumb; ){
+	// 	numb = prompt ("Введіть число більше ніж "+ firstNumb);
+	// }
+
+
+//вивід натуральних чисел в інтервалі введених користувачем
+
+	// function getNamber(){
+	// 	var a = prompt ("Введіть перше число", 0),
+	//  	b = prompt ("Введіть друге число ", 0), i=0, j=0,
+	//  	string="";
+	//  	for (i=a; i<=b; i++){
+	//  		for (j=2; j<i; j++){
+	//  			if(i % j == 0){
+	//  				continue;
+	//  			}else{
+	//  				string += i+"; ";
+	//  				break;
+	//  			}
+	//  		}
+	//  	}
+	//  	console.log(string);
+	// }
+	// document.addEventListener("DOMContentLoaded", getNamber);
+})();
